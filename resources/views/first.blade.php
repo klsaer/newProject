bcc129f151813c0347daeb20bc9c7fb3257368b6
Студенческая <@rbiu.local>
2024-09-23 12:30:23 +0500

    0.1

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
+@extends('layouts.main')
+@section('content')
     <h1>Pervaya </h1>    
     <a href="{{route('home')}}"> На первую</a><br>;
 
@@ -17,6 +10,4 @@
     @endif
     </p>
     @endfor
-</body>
-
-</html>
+@endsection()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
+@extends('layouts.main')
+@section('content')
     <h1>Главная страница </h1>    
     <a href="{{route('first')}}"> На первую</a>
-</body>
-</html>
+@endsection()
