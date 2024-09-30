cd3c90e7cb5f9a823a577148d8fe34a28d1efd2c
klsaer <jedismooth.inc@mail.ru>
2024-09-30 12:51:49 +0500

    create students.index and install tailwind

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\StudentController;
+
  
 
 Route::get('/', [MainController::class, 'show'])->name('home');
 
 Route::get('/first', [MainController::class, 'showw'])->name('first');
+
+Route::get('/students', [StudentController::class, 'index']) 
+        ->name('students.index');
